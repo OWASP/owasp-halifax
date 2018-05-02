fcb6b122cddcbc6deaf49d63d6c7ff013f34f0f7
Lilly Chalupowski <lillypadgirl86@gmail.com>
2018-05-02 17:11:20 -0300

    update

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 cd _site/ && \
     mkdir owasp-halifax/ && \
     mv assets/ owasp-halifax/ && \
-    mv pages/ owasp-halifax/
+    mv pages/ owasp-halifax/ && \
+    cp index.html owasp-halifax
 
 cd owasp-halifax/pages/ && \
     find . -name "*.html" | while read i; do
