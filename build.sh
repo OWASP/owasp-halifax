c8774b53cc4013a1bc658b2612041728caa3d957
Evan Lowry <lowry.e@gmail.com>
2018-05-30 06:45:21 -0300

    Use relative_url to clean up local site dev
    This undoes the hardcoded owasp-halifax url prefixes, which removes the
    need for the extra build.sh script.
    
    Since jekyll 3.3's release, github pages has automatically set the
    baseurl value for a website.
    https://blog.github.com/2016-11-02-what-s-new-in-github-pages-with-jekyll-3-3/

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd _site/ && \
-    mkdir owasp-halifax/ && \
-    mv assets/ owasp-halifax/ && \
-    mv pages/ owasp-halifax/ && \
-    cp index.html owasp-halifax && \
-    mv 2018/ owasp-halifax/
