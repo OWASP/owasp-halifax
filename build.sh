0e0d2c93ba19c46b1ae5e5f194f6446769759264
Evan Lowry <lowry.e@gmail.com>
2018-05-30 06:18:38 -0300

    Use 'pretty' urls in front-matter
    When a page permalink ends with a slash, jeykll falls into 'pretty' url
    mode, and creates a `foo/index.html` file instead of a `foo.html` file.
    
    This removes the need for the post-build path manging in the build.sh
    script.

--- build.sh
+++ build.sh
@@ -6,10 +6,3 @@ cd _site/ && \
     mv pages/ owasp-halifax/ && \
     cp index.html owasp-halifax && \
     mv 2018/ owasp-halifax/
-
-cd owasp-halifax/pages/ && \
-    find . -name "*.html" | while read i; do
-        d=$(echo $i | sed 's/.\{5\}$//');
-        mkdir $d;
-        mv $i $d/index.html;
-    done
